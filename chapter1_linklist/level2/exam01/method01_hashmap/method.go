eb09feb110d1dbdece1ac8239b043786dfd0a23d
小熊 <>
2023-07-19 14:31:38 +0800

    代码修改：比较是否为公共节点，需要使用该节点的地址进行比对

--- chapter1_linklist/level2/exam01/method01_hashmap/method.go
+++ chapter1_linklist/level2/exam01/method01_hashmap/method.go
@@ -2,43 +2,45 @@ package method01_hashmap
 
 import (
 	slink "xj/chapter1_linklist/level2/exam01/single_linklist"
+	// "fmt"
 )
 
 // 使用hashMap
-// 返回找到的公共结点，没找到时返回list1
+// 返回找到的公共结点，没找到时返回nil
 func Method(list1 *slink.Cat, list2 *slink.Cat) (bool, *slink.Cat){
 	if list1.Next == nil || list2.Next == nil {
 		return false, list1
 	}
-	list1_map := make(map[int]*slink.Cat)
+	list1_map := make(map[*slink.Cat]*slink.Cat)
+	// 这里单向链表，head头结点不存数据，直接跳过
 	list1_tmp := list1.Next
 	// 遍历list1，放到hashmap
 	for {
-		list1_map[list1_tmp.Id] = list1_tmp
+		list1_map[list1_tmp] = list1_tmp
 		if list1_tmp.Next == nil {
 			break
 		}
 		list1_tmp = list1_tmp.Next
 	}
 	find := false
+	// 同理：单向链表，head头结点不存数据，直接跳过
 	list2_tmp := list2.Next
 	// 遍历list2，找到hashmap中存在的点 就是公共结点
 	for {
-		_, ok := list1_map[list2_tmp.Id]
+		_, ok := list1_map[list2_tmp]
 		if ok {
 			find = true
 			break
-		} else {
-			list2_tmp = list2_tmp.Next
 		}
-		if list2_tmp == nil {
+		if list2_tmp.Next == nil {
 			break
 		}
+		list2_tmp = list2_tmp.Next
 	}
 	if find {
 		// fmt.Printf("找到的公共点的Id为 %d \n", list2_tmp.Id)
-		return find, list2_tmp
+		return find, list2_tmp	// 返回公共节点
 	}
 	// fmt.Println("这两个链表没有公共的点")
-	return find, list1
+	return find, nil
 }
--- chapter1_linklist/level2/exam01/method01_hashmap/method_test.go
+++ chapter1_linklist/level2/exam01/method01_hashmap/method_test.go
@@ -9,20 +9,40 @@ import (
 
 func TestMethod(t *testing.T) {
 	fmt.Printf("======= %s =======\n", "测试可以找到的数据")
-	list1 := slink.BuildList(1, 2, 30, 40, 50)
+	node1 := &slink.Cat{Id: 1}
+	node2 := &slink.Cat{Id: 2}
+	node3 := &slink.Cat{Id: 30}
+	node4 := &slink.Cat{Id: 40}
+	node5 := &slink.Cat{Id: 50}
+	list1 := &slink.Cat{}
+	list1.Next = node1
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node5
+	node8 := &slink.Cat{Id: 8}
+	node9 := &slink.Cat{Id: 9}
+	node10 := &slink.Cat{Id: 10}
+	list2 := &slink.Cat{}
+	list2.Next = node8
+	node8.Next = node9
+	node9.Next = node10
+	node10.Next = node3
+	node3.Next = node4
+	node4.Next = node5
+
 	fmt.Println(slink.DumpList(list1))
-	list2 := slink.BuildList(8, 9, 10, 30, 40, 50)
 	fmt.Println(slink.DumpList(list2))
 
 	find, commonNode := Method(list1, list2)
 	if find && commonNode.Id == 30 {
-		fmt.Printf("except\t%v \n got\t%v \n", commonNode.Id, 30)
+		fmt.Printf("except\t%v \n got\t%v \n", 30, commonNode.Id)
 	} else {
-		t.Errorf("\n except\t%v \n got\t%v \n", commonNode.Id, 30)
+		t.Errorf("\n except\t%v \n got\t%v \n", 30, commonNode)
 	}
 
 	fmt.Printf("======= %s =======\n", "测试找不到的数据")
-	list3 := slink.BuildList(1, 2, 300, 400, 500)
+	list3 := slink.BuildList(1, 2, 30, 40, 50)
 	fmt.Println(slink.DumpList(list3))
 	list4 := slink.BuildList(8, 9, 10, 30, 40, 50)
 	fmt.Println(slink.DumpList(list4))
