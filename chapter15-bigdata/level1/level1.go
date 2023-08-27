2f99cd7fcd79184f75b1df0dafb205bbf155c54d
小熊 <627516430@qq.com>
2023-08-27 21:31:34 +0800

    更新代码

--- chapter15-bigdata/level1/level1.go
+++ chapter15-bigdata/level1/level1.go
@@ -1,40 +1,40 @@
 package level1
 
-func FindDuplicateByByteIn32000(arr []int) (duplicate []int) {
+func FindDuplicatesByByteIn32000(arr []int) (duplicate []int) {
 	// 32000个元素
 	// 4KB = 4*1024(byte) = 4*1024*8(bit) = 32768(bit) > 32000
 	// 所以可以使用 32000(bit) 存储 这32000个元素的存在状态
 	N := 32000
-	byteMap := make([]byte, N/8+1)
+	bitmap := make([]byte, N/8+1)
 	for _, num := range arr {
-		// 计算 num 在 byteMap 中的索引
+		// 计算 num 在 bitmap 中的索引
 		// index := num / 8
 		index := num >> 3
-		// 计算 num 在 byteMap 中的偏移量
+		// 计算 num 在 bitmap 中的偏移量
 		offset := num % 8
 		mark := byte(1 << offset)
-		if byteMap[index]&mark != 0 {
+		if bitmap[index]&mark != 0 {
 			duplicate = append(duplicate, num)
 		} else {
-			byteMap[index] |= mark
+			bitmap[index] |= mark
 		}
 	}
 	return
 }
 
-func FindDuplicateByIntIn32000(arr []int) (duplicate []int) {
+func FindDuplicatesByIntIn32000(arr []int) (duplicate []int) {
 	N := 32000
 	// 或者这里不用+1，只要索引是base0的即可
-	byteMap := make([]int, N/32)
+	bitmap := make([]int, N/32)
 	for _, num := range arr {
 		num0 := num - 1 //base0开始
 		index := num0 / 32
 		offset := num0 % 32
 		mark := 1 << offset
-		if byteMap[index]&mark != 0 {
+		if bitmap[index]&mark != 0 {
 			duplicate = append(duplicate, num)
 		} else {
-			byteMap[index] |= mark
+			bitmap[index] |= mark
 		}
 	}
 	return
--- chapter15-bigdata/level1/level1_test.go
+++ chapter15-bigdata/level1/level1_test.go
@@ -16,14 +16,14 @@ func equal[T string | int | []int](t *testing.T, desc string, str1 T, str2 T) {
 	fmt.Println("")
 }
 
-func TestFindDuplicateByByteIn32000(t *testing.T) {
+func TestFindDuplicatesByByteIn32000(t *testing.T) {
 	arr := []int{1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8, 9, 10, 15, 15, 30, 30, 45, 99, 99, 100}
-	res := FindDuplicateByByteIn32000(arr)
-	equal[[]int](t, "FindDuplicateByByteIn32000", res, []int{3, 6, 7, 15, 30, 99})
+	res := FindDuplicatesByByteIn32000(arr)
+	equal[[]int](t, "FindDuplicatesByByteIn32000", res, []int{3, 6, 7, 15, 30, 99})
 }
 
-func TestFindDuplicateByIntIn32000(t *testing.T) {
+func TestFindDuplicatesByIntIn32000(t *testing.T) {
 	arr := []int{1, 2, 3, 3, 4, 5, 6, 6, 7, 7, 8, 9, 10, 15, 15, 30, 30, 45, 99, 99, 100}
-	res := FindDuplicateByIntIn32000(arr)
-	equal[[]int](t, "FindDuplicateByIntIn32000", res, []int{3, 6, 7, 15, 30, 99})
+	res := FindDuplicatesByIntIn32000(arr)
+	equal[[]int](t, "FindDuplicatesByIntIn32000", res, []int{3, 6, 7, 15, 30, 99})
 }
